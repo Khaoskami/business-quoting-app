3d68daaec0ab4c5e268cbea6b98980cbbe9487ef
Khaoskami <166114763+Khaoskami@users.noreply.github.com>
2026-09-18 20:26:33 +0200

    wipe

--- server/db/migrations/0004_romantic_preak.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX "catalog_items_user_id_idx" ON "catalog_items" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "clients_user_id_idx" ON "clients" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "invoices_user_id_idx" ON "invoices" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "quotes_user_id_created_at_idx" ON "quotes" USING btree ("user_id","created_at");
--- server/db/migrations/0007_invoice_due_index.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX IF NOT EXISTS "invoices_due_at_idx" ON "invoices" ("due_at");
